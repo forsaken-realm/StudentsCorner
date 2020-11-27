ff2883796ff8051984c84c56bad7af78f1dbd7cc
forsaken-realm <64582408+forsaken-realm@users.noreply.github.com>
2020-11-27 15:15:15 +0530

    Addded Pop up form

--- Dashboard/HTML_PHP/announcement.php
+++ Dashboard/HTML_PHP/announcement.php
@@ -13,18 +13,68 @@ include('dashBoard.php');
         }
     </style>
 </head>
+<script src="https://kit.fontawesome.com/b5cff000aa.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <link rel="stylesheet" href="../css/announcement.css">
-<body style="background-color:  #fffcfc">
-    
+<body style="background-color:  #fffcfc;scroll-behavior: smooth;" id="my-body">
+
+
+<!--This code is about announcement pop up-->
+<div id="my-overlay" ">
+    <div class="my-form-popup" id="myForm">
+        <form action="" class="form-container">
+            <h1>Announce</h1>
+            <div class="form-group" style="width: 300px">
+                <label for="exampleFormControlTextarea1">Input Text</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputState">Semester</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose semester</option>
+                        <option>I</option>
+                        <option>II</option>
+                        <option>III</option>
+                        <option>IV</option>
+                        <option>V</option>
+                        <option>VI</option>
+                        <option>VII</option>
+                        <option>VIII</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputState">Section</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>A</option>
+                        <option>B</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button onclick="off()" type="button" class="btn btn-primary btn-sm bg-dark" style="background-color: #181717">Cancel</button>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-primary btn-sm bg">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+<!--This is code is for Announcement Card-->
     <section id="cards">
         <div class="container py-2">
             <div class="d-flex flex-column flex-wrap align-items-center ">
                 <div class="col-lg-4 col-md-6 mb-4  ">
-                    <div class="shadow-sm border-0 rounded" style="width: 600px;height: 400px;background-color: #2f2f30;margin-bottom: 100px;">
+                    <div class="shadow-sm border-0 rounded" style="width: 600px;height: 300px;background-color: #2f2f30;">
                         <div class="card-body">
-                            <div class="user-picture">
-                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="shadow-sm rounded-circle" height="80" width="80"style="padding-bottom: 10px" />
+                            <div class="my-user-profile-class float-left">
+                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="" height="90" width="90"style="border-radius: 45px;" />
                             </div>
                             <div class="user-content">
                                 <h5 class="text-capitalize user-name" style="color: white">Carry Johnshon</h5>
@@ -43,10 +93,10 @@ include('dashBoard.php');
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4  ">
-                    <div class="shadow-sm border-0 rounded" style="width: 600px;height: 400px;background-color:#2f2f30;margin-bottom: 100px;">
+                    <div class="shadow-sm border-0 rounded" style="width: 600px;height: 300px;background-color:#2f2f30;margin-bottom: 100px;">
                         <div class="card-body">
-                            <div class="user-picture">
-                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="shadow-sm rounded-circle" height="130" width="130"style="padding-bottom: 10px" />
+                            <div class="my-user-profile-class float-left">
+                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="shadow-sm rounded-circle" height="90" width="90"/>
                             </div>
                             <div class="user-content">
                                 <h5 class="text-capitalize text-light user-name">Carry Johnshon</h5>
@@ -67,10 +117,27 @@ include('dashBoard.php');
             </div>
         </div>
     </section>
-    <div class="chat-box-container" style="margin-left: 36.5%;">
-        <textarea rows="2" placeholder="Your message" style="text-align: justify"></textarea>
-        <button class="btn-send-message">Send</button>
+
+
+
+
+<!--this code is for Floating button for pop up form -->
+    <div class="my-pop-up-button" onclick="on()">
+        <i class="fas fa-plus" style="padding-top: 20px;"></i>
     </div>
+
+
+<script>
+    function on() {
+        document.getElementById("my-overlay").style.display = "block";
+        // document.getElementById("my-body").style.overflow = "hidden";
+    }
+
+    function off() {
+        document.getElementById("my-overlay").style.display = "none";
+        // document.getElementById("my-body").style.overflow = "scroll";
+    }
+</script>
 </body>
 
 </html>
