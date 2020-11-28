b9aed3c1f1cb90dcf24793b49917699f0fc167e1
forsaken-realm <64582408+forsaken-realm@users.noreply.github.com>
2020-11-28 11:59:07 +0530

    added dropdown menus

--- Dashboard/HTML_PHP/announcement.php
+++ Dashboard/HTML_PHP/announcement.php
@@ -24,9 +24,20 @@ include('dashBoard.php');
     <div class="my-form-popup" id="myForm">
         <form action="" class="form-container">
             <h1>Announce</h1>
-            <div class="form-group" style="width: 300px">
+            <div class="form-group" style="width: 360px">
                 <label for="exampleFormControlTextarea1">Input Text</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="inputState">Choose Department</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>Computer Science Engineering</option>
+                    <option>Electronic and Communication Engineering </option>
+                    <option>Electronics and Electrical Engineering</option>
+                    <option>Mechanical Engineering</option>
+                    <option>Information and Science Engineering</option>
+                </select>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
@@ -71,21 +82,16 @@ include('dashBoard.php');
         <div class="container py-2">
             <div class="d-flex flex-column flex-wrap align-items-center ">
                 <div class="col-lg-4 col-md-6 mb-4  ">
-                    <div class="shadow-sm border-0 rounded" style="width: 600px;height: 300px;background-color: #2f2f30;">
+                    <div class="shadow-sm border-0 rounded" style="width: 600px;background-color: #2f2f30;">
                         <div class="card-body">
                             <div class="my-user-profile-class float-left">
-                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="" height="90" width="90"style="border-radius: 45px;" />
+                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="" height="40" width="40"style="border-radius: 20px;" />
                             </div>
+                            
                             <div class="user-content">
-                                <h5 class="text-capitalize user-name" style="color: white">Carry Johnshon</h5>
-                                <p class=" text-capitalize text-light small blockquote-footer " >Web developer</p>
-<!--                                <div class="small">-->
-<!--                                    <i class="fas fa-star text-warning"></i>-->
-<!--                                    <i class="fas fa-star text-warning"></i>-->
-<!--                                    <i class="fas fa-star text-warning"></i>-->
-<!--                                    <i class="fas fa-star-half-alt text-warning"></i>-->
-<!--                                    <i class="fas fa-star text-light"></i>-->
-<!--                                </div>-->
+                                <h6 class="text-capitalize user-name" style="color: white">Carry Johnshon</h6>
+                                <p class=" text-capitalize text-light small blockquote-footer " >Computer Science Department</p>
+                                <hr style="width: 300px;height:1px;border:none;background-color:white">
                                 <p class="small text-light mb-0 " >Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum
                                     quia commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, soluta perferendis! Iusto sequi, quae sunt iste voluptate rerum nulla perspiciatis accusantium cupiditate, officia reprehenderit sit, architecto totam quo eius dignissimos! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum animi, dolorum odio error voluptas quis, veritatis quisquam distinctio, molestias eligendi enim ipsam cum pariatur optio! Amet accusamus maxime voluptatem vel.</p>
                             </div>
@@ -93,23 +99,20 @@ include('dashBoard.php');
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4  ">
-                    <div class="shadow-sm border-0 rounded" style="width: 600px;height: 300px;background-color:#2f2f30;margin-bottom: 100px;">
+                    <div class="shadow-sm border-0 rounded" style="width: 600px;background-color:#2f2f30;margin-bottom: 100px;">
                         <div class="card-body">
                             <div class="my-user-profile-class float-left">
-                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="shadow-sm rounded-circle" height="90" width="90"/>
+                                <img src="https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130" class="" height="40" width="40" style="border-radius: 20px;"/>
                             </div>
                             <div class="user-content">
-                                <h5 class="text-capitalize text-light user-name">Carry Johnshon</h5>
-                                <p class=" text-capitalize text-light small blockquote-footer">Web developer</p>
-                                <!--                                <div class="small">-->
-                                <!--                                    <i class="fas fa-star text-warning"></i>-->
-                                <!--                                    <i class="fas fa-star text-warning"></i>-->
-                                <!--                                    <i class="fas fa-star text-warning"></i>-->
-                                <!--                                    <i class="fas fa-star-half-alt text-warning"></i>-->
-                                <!--                                    <i class="fas fa-star text-light"></i>-->
-                                <!--                                </div>-->
+                                <h6 class="text-capitalize text-light user-name">Carry Johnshon</h6>
+                                <p class=" text-capitalize text-light small blockquote-footer">Electronics and communication department</p>
+                                <hr style="width: 300px;height:1px;border:none;background-color:white">
                                 <p class="small text-light mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum
-                                    quia commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, soluta perferendis! Iusto sequi, quae sunt iste voluptate rerum nulla perspiciatis accusantium cupiditate, officia reprehenderit sit, architecto totam quo eius dignissimos! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum animi, dolorum odio error voluptas quis, veritatis quisquam distinctio, molestias eligendi enim ipsam cum pariatur optio! Amet accusamus maxime voluptatem vel.</p>
+                                    quia commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, soluta perferendis! Iusto sequi, quae sunt iste voluptate rerum nulla perspiciatis accusantium cupiditate, officia reprehenderit sit, architecto totam quo eius dignissimos! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum animi, dolorum odio error voluptas quis, veritatis quisquam distinctio, molestias eligendi enim ipsam cum pariatur optio! Amet accusamus maxime voluptatem vel.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum
+                                    quia commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, soluta perferendis! Iusto sequi, quae sunt iste voluptate rerum nulla perspiciatis accusantium cupiditate, officia reprehenderit sit, architecto totam quo eius dignissimos! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum animi, dolorum odio error voluptas quis, veritatis quisquam distinctio, molestias eligendi enim ipsam cum pariatur optio! Amet accusamus maxime voluptatem vel.
+                                </p>
                             </div>
                         </div>
                     </div>
